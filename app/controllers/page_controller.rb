dde8688280728d8dabb9a19655fa00c32eff5a5a
Yusuke Oshiro <yusuke@oshiro1.com>
2016-08-21 15:02:35 +0900

    fixes timezone problem

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -15,11 +15,22 @@ class PageController < ApplicationController
 		user = payload["targetUser"]
 
 		cal = Icalendar::Calendar.new
+
+		cal.timezone do |t|
+		  t.tzid = "Etc/UTC"
+		  t.standard do |s|
+		    s.tzoffsetfrom = "-0000"
+		    s.tzoffsetto   = "-0000"
+		    s.tzname       = "UTC"
+		  end
+		end
+
+
 		cal.event do |e|
 			p event["From1__c"]
 			p Time.parse( event["From1__c"] ).to_datetime
-			e.dtstart     = Time.parse( event["From1__c"] ).to_datetime
-			e.dtend       = Time.parse( event["To1__c"] ).to_datetime
+			e.dtstart     = Time.zone.parse( event["From1__c"] )
+			e.dtend       = Time.zone.parse( event["To1__c"] )
 			e.summary     = event["Name"]
 			e.description = event["Description__c"]
 			e.ip_class    = "PUBLIC"
@@ -27,9 +38,13 @@ class PageController < ApplicationController
 			e.sequence    = 0
 			e.priority    = 5
 			e.transp      = "OPAQUE"
+			e.uid         = event["Id"]
 			e.attendee = Icalendar::Values::CalAddress.new("mailto:"+user["Email"], cn: user["Name"])
 		end
 		cal.ip_method = "REQUEST"
+
+		p cal.to_ical
+
 
 		EventInvitation.new_event(user["Email"], cal.to_ical , event["Name"] ).deliver_now
 		render :json => nil
