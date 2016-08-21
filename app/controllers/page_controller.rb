3ac8d6e3736ca868301e0abfc62c37b38b57d106
Yusuke Oshiro <yusuke@oshiro1.com>
2016-08-21 12:30:48 +0900

    added email sending and ical generating feature

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,38 @@
+require "pp"
+require 'icalendar'
+require 'time'
+
+
 class PageController < ApplicationController
-  def send_invite
-  	EventInvitation.new_event("yusuke@oshiro1.com", "aa","new event!").deliver_now
-  	render :json => nil
-  end
+	skip_before_filter :verify_authenticity_token
+
+	
+	def send_invite
+
+		# get the payload 
+		payload = JSON.parse(params["payload"])
+		event = payload["training"]
+		user = payload["targetUser"]
+
+		cal = Icalendar::Calendar.new
+		cal.event do |e|
+			p event["From1__c"]
+			p Time.parse( event["From1__c"] ).to_datetime
+			e.dtstart     = Time.parse( event["From1__c"] ).to_datetime
+			e.dtend       = Time.parse( event["To1__c"] ).to_datetime
+			e.summary     = event["Name"]
+			e.description = event["Description__c"]
+			e.ip_class    = "PUBLIC"
+			e.status      = "CONFIRMED"
+			e.sequence    = 0
+			e.priority    = 5
+			e.transp      = "OPAQUE"
+			e.attendee = Icalendar::Values::CalAddress.new("mailto:"+user["Email"], cn: user["Name"])
+		end
+
+		#cal.publish
+		cal.ip_method = "REQUEST"
+		EventInvitation.new_event(user["Email"], cal.to_ical , event["Name"] ).deliver_now
+		render :json => nil
+	end
 end
--- app/mailers/event_invitation.rb
+++ app/mailers/event_invitation.rb
@@ -3,7 +3,7 @@ class EventInvitation < ApplicationMailer
 
 	def new_event( email, ics_content, event_name )
 	    #attachments['invite.ics'] = File.read('invite.ics')
+	    attachments['invite.ics'] = ics_content
 	    mail(to: email, subject: 'トレーニング招待: ' + event_name)
 	end
-
 end
